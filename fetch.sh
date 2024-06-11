fdbc5cacf1e2fc4cf213c9cfa124ea429725fcd4
Sacha Wheeler <sacha@sachawheeler.com>
2024-06-11 11:50:26 +0100

    decode URL

--- fetch.sh
+++ fetch.sh
@@ -11,12 +11,18 @@ curl -s "$XML_URL" -o temp.xml
 # Parse the XML file for links
 links=$(xmllint --xpath '//link' temp.xml | sed 's/ link="/\n/g' | sed 's/"//g' | tail -n +2)
 
+function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
+
 # Check each link and add to Transmission if not already added
 for link in $links; do
     if ! grep -q "$link" "$LOG_FILE"; then
         echo "Adding $link to Transmission..."
         magnet_link=$(echo "$link" | sed -e 's/^<[^>]*>//' -e 's/<[^>]*>$//')
-        transmission-remote -a "$magnet_link"
+        magnet=$(urldecode "$magnet_link")
+        url=$(echo "$magnet" | sed 's/&amp;/\&/g')
+        echo "$url"
+        echo
+        transmission-remote -a "$url"
         echo "$link" >> "$LOG_FILE"
     else
         echo "$link already added."
